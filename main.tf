751e06d23d1aaeb7d1f819813f2313a3d4bd5f11
bjakabgit <jakabbalazsmobil@gmail.com>
2024-10-23 09:35:29 +0200

    point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ACG_Terraform_Demos_JB"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
